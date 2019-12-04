2c38efb79d46dbfb83876118d1d330dad92a8c0e
Takefumi MIYOSHI <miyo@wasamon.net>
2019-12-04 18:35:31 +0900

    copy an example qasm script written in the top page of OpenQASM

--- qasm_parser.py
+++ qasm_parser.py
@@ -132,7 +132,30 @@ if __name__ == "__main__":
     // c++-style comment
     include "qel ib1.inc"; // a space in quoted string
     '''))
-    
+
+    print(syntax.parseString('''
+// Repetition code syndrome measurement
+OPENQASM 2.0;
+include "qelib1.inc";
+qreg q[3];
+qreg a[2];
+creg c[3];
+creg syn[2];
+gate syndrome d1,d2,d3,a1,a2
+{
+  cx d1,a1; cx d2,a1;
+  cx d2,a2; cx d3,a2;
+}
+x q[0]; // error
+barrier q;
+syndrome q[0],q[1],q[2],a[0],a[1];
+measure a -> syn;
+if(syn==1) x q[0];
+if(syn==2) x q[2];
+if(syn==3) x q[1];
+measure q -> c;
+'''))
+          
     # print(exp.parseString("pi"))
     # print(exp.parseString("1.0"))
     # print(exp.parseString("1"))
