87633b5e29a05f6bd268932ed14dc51d6b9a0f7f
Takefumi MIYOSHI <miyo@wasamon.net>
2019-12-04 16:53:42 +0900

    bugfix goplist

--- qasm_parser.py
+++ qasm_parser.py
@@ -35,12 +35,8 @@ def Syntax():
            | "measure" + argument + "-" + ">" + argument + ";"
            | "reset" + argument + ";"
     )
-    goplist = pp.Forward()
-    goplist << (uop
-                | "barrier" + idlist + ";"
-                | goplist + uop
-                | goplist + "barrier" + idlist + ";"
-    )
+    gop = uop | "barrier" + idlist + ";"
+    goplist = pp.OneOrMore(gop)
     gatedecl = ("gate" + ident + idlist + "{"
                 | "gate" + ident + "(" + ")" + idlist + "{"
                 | "gate" + ident + "(" + idlist + ")" + idlist + "{"
@@ -66,6 +62,49 @@ def Syntax():
 syntax = Syntax()
 
 print(syntax.parseString("OPENQASM 1.0; qreg hoge[10];"))
+print(syntax.parseString('''
+OPENQASM 1.0;
+qreg hoge[10];
+'''))
+
+print(syntax.parseString('''
+OPENQASM 2.0;
+qreg q[3];
+qreg a[2];
+creg c[3];
+creg syn[2];
+x q[0];
+barrier q;
+syndrome q[0],q[1],q[2],a[0],a[1];
+measure a -> syn;
+if(syn==1) x q[0];
+if(syn==2) x q[2];
+if(syn==3) x q[1];
+measure q -> c;
+'''
+))
+
+print(syntax.parseString('''
+OPENQASM 2.0;
+qreg q[3];
+qreg a[2];
+creg c[3];
+creg syn[2];
+gate syndrome d1,d2,d3,a1,a2
+{
+  cx d1,a1; cx d2,a1;
+  cx d2,a2; cx d3,a2;
+}
+x q[0];
+barrier q;
+syndrome q[0],q[1],q[2],a[0],a[1];
+measure a -> syn;
+if(syn==1) x q[0];
+if(syn==2) x q[2];
+if(syn==3) x q[1];
+measure q -> c;
+'''
+))
 
 # print(syntax.parseString("pi"))
 # print(syntax.parseString("1.0"))
